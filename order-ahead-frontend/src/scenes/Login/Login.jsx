4a0e5b7a307e8a701d61c7e1c9a83ce2c9a470d5
Mario Krstevski <Mario.Krstevski@netcetera.com>
2019-01-25 18:42:13 +0100

    [Login] Created basic onclick to redirect(login)

--- order-ahead-frontend/src/scenes/Login/Login.jsx
+++ order-ahead-frontend/src/scenes/Login/Login.jsx
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
-import 'login.styles.scss';
+import './login.styles.scss';
+import styled from 'styled-components';
+import { Redirect } from 'react-router-dom'
 
+const LoginWrapper = styled.div`
+    /* border: 1px solid black; */
+`;
 class Login extends Component{
+    state = {
+        redirect: false
+    }
+    setRedirect = () => {
+        this.setState({
+            redirect: true
+        })
+    }
+    renderRedirect = () => {
+       return  (this.state.redirect) &&
+          <Redirect to='/options' />
+      }
     render(){
         return(
-            <div>
+            <LoginWrapper>
                 Login
-            </div>
+                {this.renderRedirect()}
+            <button onClick={this.setRedirect}> Login </button>
+            </LoginWrapper>
         )
     }
 }
