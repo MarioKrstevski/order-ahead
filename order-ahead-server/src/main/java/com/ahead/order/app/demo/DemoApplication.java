3eaab12cf3601206127879c7568ccd2a6cc69395
Mario Krstevski <mariokrstevski@hotmail.com>
2019-07-11 23:53:52 +0200

    Quick test if server works

--- order-ahead-server/src/main/java/com/ahead/order/app/demo/DemoApplication.java
+++ order-ahead-server/src/main/java/com/ahead/order/app/demo/DemoApplication.java
@@ -1,7 +1,23 @@
 package com.ahead.order.app.demo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+import org.springframework.boot.CommandLineRunner;
+
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +26,61 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+}
+@Entity
+class Beer {
+
+	@Id
+	@GeneratedValue
+	private long id;
+	private String name;
+
+	public Beer() {}
+
+	public Beer(String name) {
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Beer{" +
+				"id=" + id +
+				", name='" + name + '\'' +
+				'}';
+	}
+}
+
+interface  BeerRepository extends JpaRepository<Beer, Long>{
+
+}
+
+@Component
+class BeerCommandLineRunner implements CommandLineRunner{
+	private final BeerRepository repository;
+
+	public BeerCommandLineRunner(BeerRepository repository){
+		this.repository= repository;
+	}
+
+	@Override
+	public void run(String... strings) throws Exception{
+		 Stream.of("Skopsko", "Lashko", "Tuborg", "Krali Marko", "Corona", "Zlaten Dab").forEach(name -> repository.save(new Beer(name)));
+		repository.findAll().forEach(System.out::println);
+	}
 }
