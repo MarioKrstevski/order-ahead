4604776c9727e538f41ff37e7990ac275aa70653
Mario Krstevski <mariokrstevski@hotmail.com>
2019-07-22 02:22:19 +0200

    [Client] comment out console.logs

--- order-ahead-frontend/src/ProtectedRoute.js
+++ order-ahead-frontend/src/ProtectedRoute.js
@@ -3,9 +3,8 @@ import { AuthContext } from "./AuthContext";
 import { navigate  } from "@reach/router";
 
 function ProtectedRoute({component:Component, prevLocation, allowed, authenticatedOnly, path, ...rest}){
-
-    console.log(Component);
-    console.log('Previous Location: ', prevLocation)
+    // console.log(Component);
+    // console.log('Previous Location: ', prevLocation)
     const { user } = useContext(AuthContext);
 
     if(user.isAuthenticated !== authenticatedOnly){
