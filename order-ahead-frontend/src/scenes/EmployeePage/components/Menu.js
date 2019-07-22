e2d8b96f7242a09f856a68e026c8598c1f950c75
Mario Krstevski <mariokrstevski@hotmail.com>
2019-07-23 01:46:51 +0200

    [Client] Moved mutation inside

--- order-ahead-frontend/src/scenes/EmployeePage/components/Menu.js
+++ order-ahead-frontend/src/scenes/EmployeePage/components/Menu.js
@@ -1,14 +1,45 @@
 import React from 'react';
 
-function Menu({dailyMenu, selectedRestaurant}){
+import gql from "graphql-tag";
+import { useQuery } from "react-apollo-hooks";
 
-    const foods = dailyMenu.food.map(food => {
+
+const GET_DAILY_MENU = gql`
+  query GET_DAILY_MENU($date: String!, $restaurant: String!) {
+    getDailyMenu(date: $date, restaurant: $restaurant) {
+      food {
+        name
+        category
+      }
+      restaurant {
+        name
+      }
+      ordersNumber
+      shifts
+    }
+  }
+`;
+
+function Menu({selectedRestaurant}){
+
+    const { data, loading, error, refetch } = useQuery(GET_DAILY_MENU, {
+        variables: {
+          restaurant: selectedRestaurant,
+          date: Date().toString()
+        }
+      });
+    
+      if (loading) return "Loading daily menu...";
+      if (error) return `Error daily menu! ${error.message}`;
+
+      const foods = data.getDailyMenu.food.map(food => {
         return <div key={food.name}>
             <span>{food.category}</span>
             <span>{food.name}</span>
             <input type={"radio"} name={food.name} />
         </div>
     })
+
     return (
         <div>
             <h2>{selectedRestaurant}</h2>
