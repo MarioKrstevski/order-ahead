d23f2e9e3d53aebb34d61b320e7b7e6afa1c4fb1
Mario Krstevski <mariokrstevski@hotmail.com>
2019-07-22 20:40:25 +0200

    [Client] Changing EmployeePage to use hooks instead of render props

--- order-ahead-frontend/src/scenes/EmployeePage/EmployeePage.js
+++ order-ahead-frontend/src/scenes/EmployeePage/EmployeePage.js
@@ -4,6 +4,7 @@ import api from "../../services/api";
 
 import gql from "graphql-tag";
 import { Query } from "react-apollo";
+import { useQuery } from "react-apollo-hooks";
 
 import RestaurantPicker from "./components/RestaurantPicker";
 import Menu from "./components/Menu";
@@ -32,54 +33,45 @@ const GET_DAILY_MENU = gql`
   }
 `;
 
-function EmployeePage() {
+function GetRestaurantsQueryWrapped(){
+  const { data, error, loading } = useQuery(GET_RESTAURANTS);
+  if (loading) return "Loading restaurants...";
+  if (error) return `Error restaurants! ${error.message}`;
+
+  return <EmployeePage restaurantsQuery={data}></EmployeePage>
+
+}
+
+function EmployeePage({restaurantsQuery}) {
   const { user } = useContext(AuthContext);
   const [dailyMenu, setDailyMenu] = useState(null);
   const [menu, setMenu] = useState([]);
   const [selectedRestaurant, setSelectedRestaurant] = useState("");
 
+  const {data, loading, error, refetch} = useQuery(GET_DAILY_MENU, { variables : {restaurant: restaurantsQuery.getRestaurants[0].name, date: Date().toString()}});
+
+  console.log("RESTAURANTS: ",  restaurants);
+  console.log("DAILY MENU: ", data);
+
+  if (loading) return "Loading daily menu...";
+  if (error) return `Error daily menu! ${error.message}`;
+
+  const restaurants = restaurantsQuery.getRestaurants.map((restaurant) => {
+    return restaurant.name
+  });
+
   return (
-    <Query query={GET_RESTAURANTS}>
-      {({ loading, error, data, refetch }) => {
-        if (loading) return "Loading restaurants...";
-        if (error) return `Error restaurants! ${error.message}`;
-
-        const firstRestaurant = data.getRestaurants[0].name;
-        const restaurants = data.getRestaurants.map((restaurant) => {
-          return restaurant.name
-        });
-
-        setSelectedRestaurant(firstRestaurant);
-
-        return (
-          <Query
-            query={GET_DAILY_MENU}
-            variables={{
-              restaurant: selectedRestaurant,
-              date: new Date().toISOString()
-            }}
-          >
-            {({ loading, error, data, refetch }) => {
-              if (loading) return "Loading daily menu...";
-              if (error) return `Error daily menu! ${error.message}`;
-
-              console.log("DAILY MENU DATA :", data);
-
-              return (
-                <>
-                  <RestaurantPicker
-                    restaurants={restaurants}
-                    setSelectedRestaurant={setSelectedRestaurant}
-                    refetch={refetch}
-                  />
-                  <Menu dailyMenu={data.getDailyMenu} selectedRestaurant={selectedRestaurant} />
-                </>
-              );
-            }}
-          </Query>
-        );
-      }}
-    </Query>
+    <>
+      <RestaurantPicker
+        restaurants={restaurants}
+        setSelectedRestaurant={setSelectedRestaurant}
+        refetch={refetch}
+      />
+      <Menu
+        dailyMenu={data.getDailyMenu}
+        selectedRestaurant={selectedRestaurant}
+      />
+    </>
   );
 }
-export default EmployeePage;
+export default GetRestaurantsQueryWrapped;
