6fb91dd7a6c2f1792f01d242e562ea31b8a07217
Mario Krstevski <mariokrstevski@hotmail.com>
2019-07-22 03:55:05 +0200

    [Client] Setting up apollo around App

--- order-ahead-frontend/src/index.js
+++ order-ahead-frontend/src/index.js
@@ -4,7 +4,35 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { ApolloClient, ApolloLink  } from 'apollo-boost';
+import { onError } from 'apollo-link-error'
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloProvider } from 'react-apollo';
+
+
+
+const errorLink = onError(({ graphQLErrors }) => {
+    if (graphQLErrors) graphQLErrors.map(({ message }) => console.log(message))
+  })
+  
+  const httpLink = createHttpLink({ uri: 'http://localhost:4000/graphql' });
+  
+  const client = new ApolloClient({
+    link: ApolloLink.from([errorLink, httpLink]),
+    cache: new InMemoryCache(),
+    onError: (e) => { console.log(e) }
+  });
+  
+  const ApolloWrappedApp = () => {
+    return (
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    );
+  };
+  
+  ReactDOM.render(<ApolloWrappedApp />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
