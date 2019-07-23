7ee25290fd96ff90e589b73cedef1e63c2ef915f
Mario Krstevski <mariokrstevski@hotmail.com>
2019-07-23 02:37:52 +0200

    [Client] CSS styling

--- order-ahead-frontend/src/scenes/EmployeePage/components/Menu.js
+++ order-ahead-frontend/src/scenes/EmployeePage/components/Menu.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React from "react";
 
 import gql from "graphql-tag";
 import { useQuery } from "react-apollo-hooks";
 
-import styled from 'styled-components';
+import styled from "styled-components";
 
-const MenuWrapper = styled.div`
+const MenuWrapper = styled.div``;
 
+const InformationWrapper = styled.div`
+  border: 1px solid black;
+  text-align: left;
+  padding-left:20px;
+  margin: 10px 0;
+  & span{
+    color: #4985bd;
+  }
 `;
 
 const GET_DAILY_MENU = gql`
@@ -18,6 +26,8 @@ const GET_DAILY_MENU = gql`
       }
       restaurant {
         name
+        orderMax
+        telephone
       }
       ordersNumber
       shifts
@@ -25,44 +35,61 @@ const GET_DAILY_MENU = gql`
   }
 `;
 
-function RestaurantInformation(){
-  return "Info"
+function RestaurantInformation({
+  ordersNumber,
+  restaurant,
+  orderMax,
+  telephone
+}) {
+  let number = "+"+telephone.slice(0,3)+" "+telephone.slice(3,5)+" "+telephone.slice(5,8)+" "+telephone.slice(8,11);
+  return (
+    <InformationWrapper>
+      {ordersNumber}/{orderMax} people have ordered today at {restaurant}{" "}
+      today. <br />
+      For questions and aditional requests call {restaurant} at <span> {number} </span>
+    </InformationWrapper>
+  );
 }
-function MenuItems({foods}){
-  return foods
+function MenuItems({ foods }) {
+  return foods;
 }
-function OrderDetails(){
-  return "hey"
+function OrderDetails() {
+  return "hey";
 }
 
-function Menu({selectedRestaurant}){
+function Menu({ selectedRestaurant }) {
+  const { data, loading, error, refetch } = useQuery(GET_DAILY_MENU, {
+    variables: {
+      restaurant: selectedRestaurant,
+      date: Date().toString()
+    }
+  });
 
-    const { data, loading, error, refetch } = useQuery(GET_DAILY_MENU, {
-        variables: {
-          restaurant: selectedRestaurant,
-          date: Date().toString()
-        }
-      });
-    
-      if (loading) return "Loading daily menu...";
-      if (error) return `Error daily menu! ${error.message}`;
+  if (loading) return "Loading daily menu...";
+  if (error) return `Error daily menu! ${error.message}`;
 
-      const foods = data.getDailyMenu.food.map(food => {
-        return <div key={food.name}>
-            <span>{food.category}</span>
-            <span>{food.name}</span>
-            <input type={"radio"} name={food.name} />
-        </div>
-    })
-
+  const foods = data.getDailyMenu.food.map(food => {
     return (
-        <MenuWrapper>
-            <RestaurantInformation></RestaurantInformation>
-            <MenuItems foods={foods}></MenuItems>
-            <OrderDetails></OrderDetails>
-           
-        </MenuWrapper>
-    )
+      <div key={food.name}>
+        <span>{food.category}</span>
+        <span>{food.name}</span>
+        <input type={"radio"} name={food.name} />
+      </div>
+    );
+  });
+
+  return (
+    <MenuWrapper>
+      <RestaurantInformation
+        ordersNumber={data.getDailyMenu.ordersNumber}
+        orderMax={data.getDailyMenu.restaurant.orderMax}
+        telephone={data.getDailyMenu.restaurant.telephone}
+        restaurant={data.getDailyMenu.restaurant.name}
+      />
+      <MenuItems foods={foods} />
+      <OrderDetails />
+    </MenuWrapper>
+  );
 }
 
 export default Menu;
